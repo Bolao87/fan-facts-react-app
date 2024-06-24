937daa02086d5fed021b08605762e541e555ea05
alicia <alimcbeal1387@gmail.com>
2024-06-24 11:47:12 +0100

    added more words

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
-<div.conainer>
-  <nav>
-    <img src="react.logo.png" alt="blue and white react logo" className="nav--icon/>
-    
-    <h3 className="nav--logo_text>ReactFacts</h3>
-    <h4 className="nav--title">React Course - Project 2</h4>
-    <main>
-      <h1>Fun facts about React </h1>
-      <ul>
-        <li>Was first released in 2013</li>
-        <li>Was originally created by Jordan Walke</li>
-        <li>Has well over 100K stars on GitHub</li>
-        <li>Is maintained by Facebook</li>
-        <li>Powers thousands of enterprise apps, including mobile apps</li>
-      </ul>
 
-    </main>
-  </nav>
-</div.conainer>
+function App() {
+
+
+  return (
+
+
+
+    <div.container>
+      <nav>
+            <img src="react.logo.png" className="nav--icon" />
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
+        </nav>
+
+        <main>
+            <h1 className="main--title">Fun facts about React</h1>
+            <ul className="main--facts">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
+    </div.container>
+    )
+};
 export default App
