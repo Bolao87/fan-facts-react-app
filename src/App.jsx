3d422207bee8c81417d8d91573dde699e1b2bcfe
alicia <alimcbeal1387@gmail.com>
2024-06-24 10:17:09 +0100

    Added a new pic and page

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
+<div.conainer>
+  <nav>
+  <img src="react.logo.png" alt="blue and white react logo" />
+    
+    <h3></h3>
+    <h4></h4>
+    <main>
+      <h1>
+        <ul>
+          <li></li>
+        </ul>
+      </h1>
+    </main>
+  </nav>
+</div.conainer>
 export default App
