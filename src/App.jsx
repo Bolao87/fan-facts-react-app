4a1da6fe164060e174986db687ab9536c6034df0
alicia <alimcbeal1387@gmail.com>
2024-06-24 10:55:12 +0100

    added main

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 <div.conainer>
   <nav>
-  <img src="react.logo.png" alt="blue and white react logo"  className="nav--icon/>
+    <img src="react.logo.png" alt="blue and white react logo" className="nav--icon/>
     
     <h3 className="nav--logo_text>ReactFacts</h3>
-    <h4 className ="nav--title">React Course - Project 2</h4>
+    <h4 className="nav--title">React Course - Project 2</h4>
     <main>
-      <h1>
-        <ul>
-          <li></li>
-        </ul>
-      </h1>
+      <h1>Fun facts about React </h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on GitHub</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+
     </main>
   </nav>
 </div.conainer>
