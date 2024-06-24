3de383b29fd495e72e92b95d13c77fbbc08dc833
alicia <alimcbeal1387@gmail.com>
2024-06-24 10:46:13 +0100

    added new paragraph

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 <div.conainer>
   <nav>
-  <img src="react.logo.png" alt="blue and white react logo" />
+  <img src="react.logo.png" alt="blue and white react logo"  className="nav--icon/>
     
-    <h3></h3>
-    <h4></h4>
+    <h3 className="nav--logo_text>ReactFacts</h3>
+    <h4 className ="nav--title">React Course - Project 2</h4>
     <main>
       <h1>
         <ul>
